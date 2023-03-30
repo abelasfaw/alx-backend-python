3c70f03c3974247626305bfeffa65b217e36a641
abelasfaw <abeloman89@gmail.com>
2023-03-30 03:19:30 -0700

    fix task 6 union order

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     ''' returns sum of list with floating-point or int elements'''
     return float(sum(mxd_lst))
