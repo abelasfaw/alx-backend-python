e099ef8b69021c77af23f6f59c0f8483ddc177d0
abelasfaw <abeloman89@gmail.com>
2023-03-30 14:33:18 -0700

    fix folder for task8 and 9

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''type-annotated function make_multiplier'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''takes a float multiplier as argument and returns a function
+    that multiplies a float by multiplier.'''
+    return lambda x: x * multiplier
