1273e28e0c6f8da9013ced8995bd7fbde740db5a
abelasfaw <abeloman89@gmail.com>
2023-03-30 14:38:26 -0700

    added task 10

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''Augument code with correct duck-typed annotations'''
+from typing import Union, Any, Sequence
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''
+    Duck-typed function
+    safe_first_element
+    '''
+    if lst:
+        return lst[0]
+    else:
+        return None
